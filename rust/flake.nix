88c091034680cb80db45f020a4ea2725d2df8287
June B <jrbergeron823@gmail.com>
2025-11-03 12:34:33 -0600

    rust: Move main package stuff to `package.nix` and some other QoL stuff

--- rust/flake.nix
+++ rust/flake.nix
@@ -6,25 +6,17 @@
   };
 
   outputs = { self, nixpkgs, }: let
-    systems = [
+    forEachSystem = nixpkgs.lib.genAttrs [
       "x86_64-linux"
       "aarch64-linux"
       "x86_64-darwin"
       "aarch64-darwin"
     ];
-    forEachSystem = nixpkgs.lib.genAttrs systems;
-    pkgs = nixpkgs.legacyPackages;
-    # -- MAIN PKG --
-    mainPkg = {rustPlatform}:
-      rustPlatform.buildRustPackage {
-        pname = "rust-template";
-        version = "0.1.0";
-        src = ./.;
-        cargoLock.lockFile = ./Cargo.lock;
-      };
   in {
-    packages = forEachSystem (system: {
-      default = pkgs.${system}.callPackage mainPkg {};
+    packages = forEachSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      default = pkgs.callPackage ./package.nix {};
     });
   };
 }
---
+++ rust/package.nix
@@ -0,0 +1,10 @@
+{ lib, rustPlatform, }: let
+  toml = (lib.importTOML ./Cargo.toml).package;
+in
+  rustPlatform.buildRustPackage {
+    pname = toml.name;
+    inherit (toml) version;
+
+    src = ./.;
+    cargoLock.lockFile = ./Cargo.lock;
+  }
