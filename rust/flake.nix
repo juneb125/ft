d9e87aeac770e288935d4ddfddec5b96244fecd3
June B <jrbergeron823@gmail.com>
2025-11-08 03:51:00 -0600

    rust: Make `forEachSystem` fn better (code from <https://github.com/nix-community/nh/blob/master/flake.nix>)

--- rust/flake.nix
+++ rust/flake.nix
@@ -6,16 +6,15 @@
   };
 
   outputs = { self, nixpkgs, }: let
-    forEachSystem = nixpkgs.lib.genAttrs [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ];
+    forEachSystem = f:
+      nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ] (system: f nixpkgs.legacyPackages.${system});
   in {
-    packages = forEachSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
+    packages = forEachSystem (pkgs: {
       default = pkgs.callPackage ./package.nix {};
     });
   };
